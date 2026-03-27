794d3e11eb04016c89ef61c39472b84b47dccb35
Aiden <monoblockcorp.hbk@gmail.com>
2026-03-27 17:02:52 +0900

    Signed-off-by: Aiden <monoblockcorp.hbk@gmail.com>

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
   const params = new URLSearchParams(window.location.search);
   if (params.has("debug")) document.body.classList.add("debug");
 
-  const GAS_URL = "https://script.google.com/macros/s/AKfycbwjkiKwJ2X2G6YXkyouwd2QcX6qJGwlkk1e7i5tLUm3hVxX5RZMOV8AjdzmvRI5_stsAA/exec";
+  const GAS_URL = "https://script.google.com/macros/s/AKfycbwzOOee9dAjL4QYAxSKgw29vn1PtxsR-C48Q7b1MaTjjw8qW6SAmOG89SGU_oYelkApxg/exec";
   
   // ✅ 주말 및 공휴일을 피해 첫 영업일(오전 9시)을 찾는 함수
   const getFirstWorkingDay = (year, month) => {
